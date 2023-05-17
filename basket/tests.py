41632f3a8c3eb98067a93e2c6c0128523cbb7887
manaswitha2000 <56600071+manaswitha2000@users.noreply.github.com>
2023-05-18 00:24:40 +0530

    Add files via upload

--- basket/tests.py
+++ basket/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
-
 # Create your tests here.
+from basket.models import Basket
+
+#
+#
+class BasketTestCase(TestCase):
+    print("Testing Basket")
+    def setUp(self):
+        # created_at = models.DateTimeField(auto_now_add=True)
+        # updated_at = models.DateTimeField(auto_now=True)
+        Basket.objects.create(total_amazon = 10, total_flipkart = 20, total_jiomart = 30)
+
+    def test_basket_sum(self):
+        basket = Basket.objects.get(total_amazon = 10)
+        self.assertTrue(int(basket.total_amazon) > 0)
+        self.assertTrue(int(basket.total_flipkart) > 0)
+        self.assertTrue(int(basket.total_jiomart) > 0)
+        print("All prices are valid for bastet")
